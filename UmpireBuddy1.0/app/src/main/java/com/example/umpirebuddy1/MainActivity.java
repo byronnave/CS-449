e3477f124cfb6b6e8576926e1c363083bed38906
Byron Nave <bjnmtf@mail.umkc.edu>
2020-02-02 15:36:19 -0600

    turning in lab 1

---
+++ UmpireBuddy1.0/app/src/main/java/com/example/umpirebuddy1/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.umpirebuddy1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+private int numStrikes = 0;
+private int numBalls = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    /// wire up the button
+        /// get the button
+        ///get what happens when the user clicls
+        Button btnStrike = (Button) findViewById(R.id.btnStrike);
+
+       btnStrike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numStrikes += 1;
+                if (numStrikes > 2 ){
+                    numStrikes = 0;
+                    numBalls =0;
+                }
+                updateControlStates();
+
+
+
+
+
+            }
+        });
+        Button btnBalls = (Button) findViewById(R.id.btnBalls);
+
+        btnBalls.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numBalls += 1;
+                if (numBalls > 3 ){
+                    numBalls = 0;
+                    numStrikes =0;
+                }
+                updateControlStates();
+
+
+
+
+
+            }
+        });
+        Button btnReset = findViewById(R.id.btnReset);
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                numStrikes =0;
+                numBalls =0;
+                updateControlStates();
+            }
+
+
+        });
+
+        Button btnExit = findViewById(R.id.btnExit);
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+                homeIntent.addCategory( Intent.CATEGORY_HOME );
+                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(homeIntent);
+            }
+
+
+        });
+
+
+
+    }
+
+
+
+
+
+    private void updateControlStates(){
+        TextView textViewStrikes = findViewById(R.id.numStrikes);
+        textViewStrikes.setText(String.valueOf(numStrikes));
+        TextView textViewBalls = findViewById(R.id.numBalls);
+        textViewBalls.setText(String.valueOf(numBalls));
+
+    }
+}
